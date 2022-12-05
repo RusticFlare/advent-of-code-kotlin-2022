b7597c00cb921ccab3adc731e1e7c999682c10c3
RusticFlare <j_d_baker@hotmail.com>
2022-12-05 14:59:20 +0000

    Day 5

---
+++ src/Day05.kt
@@ -0,0 +1,51 @@
+fun main() {
+    fun part1(input: String): String {
+        val (initialState, steps) = input.split("\n\n")
+        val totalStacks = initialState.lines().last().split(" ").mapNotNull { it.toIntOrNull() }.last()
+        val stacks = (1..totalStacks)
+            .associateWith { mutableListOf<Char>() }
+        initialState.lines().forEach { line ->
+            stacks.forEach { (index, stack) ->
+                line.getOrNull((index * 4) - 3)?.takeIf { it.isLetter() }
+                    ?.let { stack.add(it) }
+            }
+        }
+        steps.lines().forEach { step ->
+            val (move, from, to) = step.split(" ").mapNotNull { it.toIntOrNull() }
+            repeat(times = move) {
+                stacks.getValue(to).add(index = 0, stacks.getValue(from).removeAt(index = 0))
+            }
+        }
+        return stacks.values.joinToString(separator = "") { it.first().toString() }
+    }
+
+    fun part2(input: String): String {
+        val (initialState, steps) = input.split("\n\n")
+        val totalStacks = initialState.lines().last().split(" ").mapNotNull { it.toIntOrNull() }.last()
+        val stacks = (1..totalStacks)
+            .associateWith { mutableListOf<Char>() }
+        initialState.lines().forEach { line ->
+            stacks.forEach { (index, stack) ->
+                line.getOrNull((index * 4) - 3)?.takeIf { it.isLetter() }
+                    ?.let { stack.add(it) }
+            }
+        }
+        steps.lines().forEach { step ->
+            val (move, from, to) = step.split(" ").mapNotNull { it.toIntOrNull() }
+            stacks.getValue(to).addAll(index = 0, stacks.getValue(from).take(move))
+                repeat(times = move) { stacks.getValue(from).removeAt(index = 0) }
+        }
+        return stacks.values.joinToString(separator = "") { it.first().toString() }
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readText("Day05_test")
+    check(part1(testInput) == "CMZ")
+    check(part2(testInput) == "MCD")
+
+    val input = readText("Day05")
+    check(part1(input) == "GFTNRBZPF")
+    println(part1(input))
+    check(part2(input) == "VRQWPDSGP")
+    println(part2(input))
+}
