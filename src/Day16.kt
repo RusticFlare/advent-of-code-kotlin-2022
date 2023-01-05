4bad1f3442d82dafd13228dc27af1df33bdcdddb
RusticFlare <j_d_baker@hotmail.com>
2023-01-05 17:06:45 +0000

    Day 16 Part 2 WIP

--- src/Day16.kt
+++ src/Day16.kt
@@ -1,42 +1,52 @@
-data class Valve(
+private data class Valve(
     val name: String,
     val flowRate: Int,
     val adjacentValves: List<String>,
 )
 
-data class State(
+private data class OneState(
     val currentValve: Valve,
     val openValves: Set<Valve> = emptySet(),
     val pressureReleased: Int = 0,
 )
 
+private data class TwoState(
+    val myValve: Valve,
+    val elephantValve: Valve,
+    val openValves: Set<Valve> = emptySet(),
+    val pressureReleased: Int = 0,
+)
+
+private fun parseValves(input: List<String>) = input.map { line ->
+    val values = line.split(
+        "Valve ",
+        " has flow rate=",
+        "; tunnels lead to valves ",
+        "; tunnel leads to valve ",
+        ", ",
+    ).drop(1)
+    Valve(
+        name = values[0],
+        flowRate = values[1].toInt(),
+        adjacentValves = values.drop(2),
+    )
+}.associateBy { it.name }
+
 fun main() {
+
     fun part1(input: List<String>): Int {
-        val valves = input.map { line ->
-            val values = line.split(
-                "Valve ",
-                " has flow rate=",
-                "; tunnels lead to valves ",
-                "; tunnel leads to valve ",
-                ", ",
-            ).drop(1)
-            Valve(
-                name = values[0],
-                flowRate = values[1].toInt(),
-                adjacentValves = values.drop(2),
-            )
-        }.associateBy { it.name }
+        val valves = parseValves(input)
 
         val maxFlowRate = valves.maxOf { (_, valve) -> valve.flowRate }
 
-        fun State.explore(): List<State> = currentValve.adjacentValves
+        fun OneState.explore(): List<OneState> = currentValve.adjacentValves
             .map { copy(currentValve = valves.getValue(it)) }
 
         return (29 downTo 1).fold(
-            initial = setOf(State(currentValve = valves.getValue("AA")))
+            initial = setOf(OneState(currentValve = valves.getValue("AA")))
         ) { states, minutesRemaining ->
 
-            fun State.openCurrentValve() : State? = if (currentValve.flowRate == 0 || currentValve in openValves) {
+            fun OneState.openCurrentValve() : OneState? = if (currentValve.flowRate == 0 || currentValve in openValves) {
                 null
             } else {
                 copy(
@@ -45,7 +55,7 @@ fun main() {
                 )
             }
 
-            fun Set<State>.cull() : List<State> {
+            fun Set<OneState>.cull() : List<OneState> {
                 val targetPressureReleased = maxOf { it.pressureReleased }
                 val possibleExtraPressureReleased = (minutesRemaining downTo 1 step 2).sum() * maxFlowRate
                 return filter { it.pressureReleased + possibleExtraPressureReleased >= targetPressureReleased }
@@ -58,21 +68,93 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val valves = parseValves(input)
+
+        val maxFlowRate = valves.maxOf { (_, valve) -> valve.flowRate }
+
+        fun TwoState.weExplore(): List<TwoState> = myValve.adjacentValves
+            .map { valves.getValue(it) }
+            .flatMap { newMyValve ->
+                elephantValve.adjacentValves
+                    .map { valves.getValue(it) }
+                    .map { newElephantValve ->
+                        copy(
+                            myValve = newMyValve,
+                            elephantValve = newElephantValve,
+                        )
+                    }
+
+            }
+
+        val aa = valves.getValue("AA")
+        return (25 downTo 1).fold(
+            initial = setOf(TwoState(myValve = aa, elephantValve = aa))
+        ) { states, minutesRemaining ->
+
+            fun TwoState.openMyValve() : List<TwoState> = if (myValve.flowRate == 0 || myValve in openValves) {
+                emptyList()
+            } else {
+                elephantValve.adjacentValves
+                    .map { valves.getValue(it) }
+                    .map { newElephantValve ->
+                        copy(
+                            openValves = openValves + myValve,
+                            pressureReleased = pressureReleased + (myValve.flowRate * minutesRemaining),
+                            elephantValve = newElephantValve,
+                        )
+                    }
+            }
+
+            fun TwoState.openElephantValve() : List<TwoState> = if (elephantValve.flowRate == 0 || elephantValve in openValves) {
+                emptyList()
+            } else {
+                myValve.adjacentValves
+                    .map { valves.getValue(it) }
+                    .map { newMyValve ->
+                        copy(
+                            openValves = openValves + elephantValve,
+                            pressureReleased = pressureReleased + (elephantValve.flowRate * minutesRemaining),
+                            myValve = newMyValve,
+                        )
+                    }
+            }
+
+            fun TwoState.openBothValves(): List<TwoState> = if (elephantValve == myValve || myValve.flowRate == 0 || elephantValve.flowRate == 0 || myValve in openValves || elephantValve in openValves) {
+                emptyList()
+            } else {
+                listOf(
+                    copy(
+                        openValves = openValves + elephantValve + myValve,
+                        pressureReleased = pressureReleased + ((elephantValve.flowRate + myValve.flowRate) * minutesRemaining),
+                    )
+                )
+            }
+
+            fun Set<TwoState>.cull() : List<TwoState> {
+                val targetPressureReleased = maxOf { it.pressureReleased }
+                val possibleExtraPressureReleased = (minutesRemaining downTo 1 step 2).sum() * maxFlowRate * 2
+                return filter { it.pressureReleased + possibleExtraPressureReleased >= targetPressureReleased }
+            }
+
+            states.cull()
+                .flatMap { state -> state.weExplore() + state.openMyValve() + state.openElephantValve() + state.openBothValves() }
+                .toSet()
+        }.maxOf { it.pressureReleased }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readLines("Day16_test")
     check(part1(testInput) == 1651)
-//    check(part2(testInput) == 4)
+    println("Test: " + part2(testInput))
+    check(part2(testInput) == 1707)
 
     val input = readLines("Day16")
     with(part1(input)) {
         check(this == 1673)
         println(this)
     }
-//    with(part2(input)) {
+    with(part2(input)) {
 //        check(this == 569)
-//        println(this)
-//    }
+        println(this)
+    }
 }
