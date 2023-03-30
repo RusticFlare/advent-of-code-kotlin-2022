1b515fb156e7f1aab4f016f080106fbf5058f590
RusticFlare <j_d_baker@hotmail.com>
2023-03-30 18:09:22 +0100

    🤬

--- src/Day22.kt
+++ src/Day22.kt
@@ -6,6 +6,7 @@ import com.github.h0tk3y.betterParse.grammar.parseToEnd
 import com.github.h0tk3y.betterParse.lexer.literalToken
 import com.github.h0tk3y.betterParse.lexer.regexToken
 import com.github.h0tk3y.betterParse.parser.Parser
+import java.util.Objects.checkIndex
 
 private enum class Spot { OPEN, WALL }
 private enum class Facing(val score: Int) { RIGHT(0), DOWN(1), LEFT(2), UP(3) }
@@ -26,6 +27,129 @@ private val pathGrammar = object : Grammar<List<Direction>>() {
     val term by move or left or right
     val terms: Parser<List<Direction>> = zeroOrMore(term)
     override val rootParser by terms
+}
+
+private class Net(val map: Map<Position, Spot>) {
+
+    data class Position(
+        val row: Int,
+        val column: Int,
+    )
+
+    inner class State(
+        val position: Position,
+        val facing: Facing,
+    ) {
+        init {
+            check(map[position] == Spot.OPEN)
+        }
+
+        val score: Int get() = (1000 * (position.row + 1)) + (4 * (position.column + 1)) + facing.score
+
+        fun ontoFaces(faces: Faces) = faces.State(
+            face = Faces.Face(
+                location = Faces.Location(
+                    row = position.row / faces.edgeSize,
+                    column = position.column / faces.edgeSize,
+                )
+            ),
+            position = Faces.Position(
+                row = position.row % faces.edgeSize,
+                column = position.column % faces.edgeSize,
+            ),
+            facing = facing,
+        )
+    }
+}
+
+private class Faces(val edgeSize: Int) {
+
+    data class Location(
+        val row: Int,
+        val column: Int,
+    )
+
+    data class Face(
+        val location: Location
+    )
+
+    data class Position(
+        val row: Int,
+        val column: Int,
+    )
+
+    inner class State(
+        val face: Face,
+        val position: Position,
+        val facing: Facing,
+    ) {
+        init {
+            checkIndex(position.row, edgeSize)
+            checkIndex(position.column, edgeSize)
+        }
+
+        fun ontoNet(net: Net) = net.State(
+            position = Net.Position(
+                row = (face.location.row * edgeSize) + position.row,
+                column = (face.location.column * edgeSize) + position.column,
+            ),
+            facing = facing,
+        )
+    }
+}
+
+private class Cube(val edgeSize: Int, faces: Collection<Faces.Face>) {
+
+    val sides: Map<CardinalDirection, Side>
+
+    init {
+        check(faces.size == 6) { faces.size }
+        mutableMapOf<CardinalDirection, Side>().apply {
+            this[Min]
+        }
+        sides = faces.first()
+    }
+
+    data class Position(
+        val x: Int,
+        val y: Int,
+        val z: Int,
+    )
+
+    class Side {
+        val face = Faces.Face
+    }
+
+    enum class CardinalDirection { X, Y, Z }
+
+    enum class UnitVector(
+        val position: Position,
+        val cardinalDirection: CardinalDirection,
+    ) {
+        PlusX(Position(x = 1, y = 0, z = 0), CardinalDirection.X),
+        PlusY(Position(x = 0, y = 1, z = 0), CardinalDirection.Y),
+        PlusZ(Position(x = 0, y = 0, z = 1), CardinalDirection.Z),
+        MinusX(Position(x = -1, y = -0, z = -0), CardinalDirection.X),
+        MinusY(Position(x = -0, y = -1, z = -0), CardinalDirection.Y),
+        MinusZ(Position(x = -0, y = -0, z = -1), CardinalDirection.Z),
+    }
+
+    inner class State(
+        val position: Position,
+        val face: UnitVector,
+        val heading: UnitVector,
+    ) {
+        init {
+            checkIndex(position.x, edgeSize)
+            checkIndex(position.y, edgeSize)
+            checkIndex(position.z, edgeSize)
+            check(
+                maxOf(position.x, position.y, position.z) == edgeSize - 1 ||
+                        minOf(position.x, position.y, position.z) == 0
+            ) { position }
+            check(face.cardinalDirection != heading.cardinalDirection)
+        }
+    }
 }
 
 fun main() {
